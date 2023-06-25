6edae3acfdb5de424da7f25053b7c7d931e0df54
Rafael Thumaza <rassraza4@gmail.com>
2023-06-24 23:35:15 -0300

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,21 @@
+
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-      extend: {
-        fontFamily: {
-        'adamcg': ['ADAMCG','sans-serif'],
-        'pt-sans': ['PT Sans', 'sans-serif'],
-        'open': ['Open Sans', 'sans-serif'],
+    extend: {
+      fontFamily: {
+        'adamcg': "ADAMCG, cursive",
+        'open': "Open Sans, sans-serif"
+
       },
-    
-        backgroundImage: {
-          'raza-logo': "url('/imagens/raza-logo-semfundo.png')",
-          'home': 'url("/imagens/download.jpeg")',
-          
-        },
+      backgroundImage: {
+        'raza-logo': "url('/imagens/raza-logo-semfundo.png')",
+        'home': "url('/imagens/download.jpeg')"
+      }
     },
-      plugins: [],
-  }
+  },
+  plugins: [],
 }
+
