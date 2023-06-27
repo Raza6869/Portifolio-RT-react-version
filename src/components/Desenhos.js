87f65d82c08e7186f0e4d6d95b37fcc0142fe176
Rafael Thumaza <rassraza4@gmail.com>
2023-06-27 10:17:30 -0300

    Updated 27 June #2

--- src/components/Desenhos.js
+++ src/components/Desenhos.js
@@ -1,14 +1,21 @@
-import ImageIlustraçaoEstilizado from "./ImageIlustEst"
-import ImageIlustraçaoSketch from "./ImageIlustSketchs"
-import ImageIlustraçaoSplash from "./ImageIlustSplash"
+import ImageGallery from "./ImageGallery"
 
 
 export default function Desenhos(){
     return(
         <div className='h-auto bg-zinc-900 bg-cover bg-no-repeat bg-[center_top_-9rem] bg-fixed py-20 px-20 '>
-            <ImageIlustraçaoEstilizado/>                   
-            <ImageIlustraçaoSplash/>   
-            <ImageIlustraçaoSketch/>                
+            <div id="nav" className="text-red-800 text-5xl flex gap-5 -ml-9 font-adamcg items-center mb-20">
+               <input type="radio" id="Color" name="color" value="color" className="invisible" defaultChecked/>
+               <label for="color">Coloridos</label>
+               <input type="radio" id="Sketchs" name="Sketchs" value="Sketchs" className="invisible"/>
+               <label for="Sketchs">Sketchs</label>
+               <input type="radio" id="Splash" name="Splash" value="Splash" className="invisible"/>
+               <label for="Splash">Splash Arts</label>
+            </div>
+                
+            <div className="grid grid-cols-3 gap-10">
+                <ImageGallery/>
+            </div>
         </div>
     )
 }
---
+++ src/components/ImageGallery.js
@@ -0,0 +1,44 @@
+import color2 from "../Desenhos/arte2.jpg"
+import color4 from "../Desenhos/arte4.jpg"
+import color5 from "../Desenhos/arte5.jpg"
+import color6 from "../Desenhos/arte6.jpg"
+import color7 from "../Desenhos/arte7.jpg"
+import color8 from "../Desenhos/arte8.jpg"
+import color10 from "../Desenhos/arte10.jpg"
+import color11 from "../Desenhos/arte11.jpg"
+import color12 from "../Desenhos/arte12.jpg"
+import color13 from "../Desenhos/arte13.jpg"
+import color14 from "../Desenhos/arte14.jpg"
+import color15 from "../Desenhos/arte15.jpg"
+import color17 from "../Desenhos/arte17.jpg"
+import color18 from "../Desenhos/arte18.jpg"
+
+export const artData = [
+    {color: color2},
+    {color: color4},
+    {color: color5},
+    {color: color6},
+    {color: color7},
+    {color: color8},
+    {color: color10},
+    {color: color11},
+    {color: color12},
+    {color: color13},
+    {color: color14},
+    {color: color15},
+    {color: color17},
+    {color: color18},
+    
+]
+
+
+
+export default function ImageGallery(){
+    return(
+        artData.map( function(item){
+            return(
+                <img src={item.color} alt="arte do raza" className="rounded-2xl flex-none "/>
+            )
+        })
+    )
+}
